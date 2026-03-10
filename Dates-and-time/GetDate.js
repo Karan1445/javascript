17ba9c631df1a092056c1f2ce58d986033120285
Karan1445 <goheljitu14@gmail.com>
2026-03-10 20:50:40 +0530

    New topic addidtion of date func's

---
+++ Dates-and-time/GetDate.js
@@ -0,0 +1,13 @@
+// The new Date() Constructor
+
+const date = new Date();
+console.log(date)
+
+console.log(date.getDate())
+console.log(date.getDay())
+console.log(date.getTimezoneOffset())//-330
+
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+console.log(months[date.getMonth()])
+
+console.log(Date.now())
