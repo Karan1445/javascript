ed8982c2320ee355ad1cd1cbbcb4b917152fb343
Kahek1814 <karan.softrefine@gmail.com>
2026-03-12 11:02:24 +0530

    const info

---
+++ Variable/const.js
@@ -0,0 +1,16 @@
+// const is hoisted and we can not change value of const but inside array we can change it
+
+const arr=[1,2,3,4]
+
+console.log(arr)
+
+arr.push(1)
+
+console.log(arr)
+
+const x=10;
+
+// now the x vlaue is 10 fixed we cannot do x=11 after its declartion
+
+// x=12; typeerror addigment to constat variable
+//its hoisted but return the refrance error
