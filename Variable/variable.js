37dc325846ddd03b4679ca60a447bdd62d33a64c
Kahek1814 <karan.softrefine@gmail.com>
2026-03-12 10:55:22 +0530

    info about varibale

---
+++ Variable/variable.js
@@ -0,0 +1,25 @@
+// JavaScript Variables
+
+// There are 3 type variable 
+
+// let var const
+// ----- LET
+
+// Variables are identified with unique names called identifiers.
+
+// we can use _ and $ in the name
+// When to Use var, let, or const?
+// 1. Always declare variables
+
+// 2. Always use const if the value should not be changed
+
+// 3. Always use const if the type should not be changed (Arrays and Objects)
+
+// 4. Only use let if you cannot use const
+
+// 5. Never use var if you can use let or const.
+// ======================================================================================
+// JavaScript variables can hold 8 datatypes, but for now, just think of numbers and strings.
+// ======================================================================================
+
+let person = "John Doe", carName = "Volvo", price = 200;//this is also a valid declaration
