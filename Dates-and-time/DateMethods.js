7d761e8690724f769e336f61991d7bc7bc1f3f45
Kahek1814 <karan.softrefine@gmail.com>
2026-03-12 10:06:36 +0530

    new commit test!

--- Dates-and-time/DateMethods.js
+++ Dates-and-time/DateMethods.js
@@ -2,4 +2,4 @@ const date=new Date();
 console.log(date.constructor)
 
 const DOB=new Date(2005,3,14)
-console.log(date.getFullYear()-DOB.getFullYear())
+console.log(date.getFullYear()-DOB.getFullYear()) 
